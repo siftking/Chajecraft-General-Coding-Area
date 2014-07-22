45ba433b177779db256de07f8d11df95f534ebca
IcyEngine <einar_d@msn.com>
2014-07-22 14:06:18 +0200

    Updated after v2.0 where added

--- ZombieRP v2/customthings/ammo.lua
+++ ZombieRP v2/customthings/ammo.lua
@@ -22,33 +22,30 @@ GAMEMODE:AddAmmoType("AR2", "Assault Rifle Ammo", "models/Items/combine_rifle_ca
 GAMEMODE:AddAmmoType("Gravity", "SMG Only Ammo", "models/Items/BoxMRounds.mdl", 60, 60)
 
 -- Pistol Ammo Types
-GAMEMODE:AddAmmoType("fas2_ammo_10x25", "10x25MM Ammo", "models/Items/BoxSRounds.mdl", 37, 30)
-GAMEMODE:AddAmmoType("fas2_ammo_45acp", ".45 ACP Ammo", "models/Items/BoxSRounds.mdl", 37, 30)
-GAMEMODE:AddAmmoType("fas2_ammo_357sig", ".357 SIG Ammo", "models/Items/BoxSRounds.mdl", 37, 30)
-GAMEMODE:AddAmmoType("fas2_ammo_9x18", "9x18MM Ammo", "models/Items/BoxSRounds.mdl", 37, 30)
+GAMEMODE:AddAmmoType("10x25MM", "10x25MM Ammo", "models/Items/BoxSRounds.mdl", 37, 30)
+GAMEMODE:AddAmmoType(".45 ACP", ".45 ACP Ammo", "models/Items/BoxSRounds.mdl", 37, 30)
+GAMEMODE:AddAmmoType(".357 SIG", ".357 SIG Ammo", "models/Items/BoxSRounds.mdl", 37, 30)
+GAMEMODE:AddAmmoType("9x18MM", "9x18MM Ammo", "models/Items/BoxSRounds.mdl", 37, 30)
 
 -- Heavy Pistol Ammo Types
-GAMEMODE:AddAmmoType("fas2_ammo_50ae", ".50 AE Ammo", "models/Items/357ammo.mdl", 50, 28)
-GAMEMODE:AddAmmoType("fas2_ammo_454casull", ".454 Casull Ammo", "models/Items/357ammo.mdl", 50, 28)
+GAMEMODE:AddAmmoType(".50 AE", ".50 AE Ammo", "models/Items/357ammo.mdl", 50, 28)
+GAMEMODE:AddAmmoType(".454 Casull", ".454 Casull Ammo", "models/Items/357ammo.mdl", 50, 28)
 
 -- Submachine Gun Ammo Tpyes
-GAMEMODE:AddAmmoType("fas2_ammo_9x19", "9x19MM Ammo", "models/Items/BoxMRounds.mdl", 80, 40)
-GAMEMODE:AddAmmoType("fas2_ammo_380acp", ".380 ACP Ammo", "models/Items/BoxMRounds.mdl", 120, 60)
+GAMEMODE:AddAmmoType("9x19MM", "9x19MM Ammo", "models/Items/BoxMRounds.mdl", 80, 40)
+GAMEMODE:AddAmmoType(".380 ACP", ".380 ACP Ammo", "models/Items/BoxMRounds.mdl", 120, 60)
 
 -- Assault Ammo Types
-GAMEMODE:AddAmmoType("fas2_ammo_545x39", "5.45x39MM Ammo", "models/Items/combine_rifle_cartridge01.mdl", 160, 60)
-GAMEMODE:AddAmmoType("fas2_ammo_762x39", "7.62x39MM Ammo", "models/Items/combine_rifle_cartridge01.mdl", 160, 60)
-GAMEMODE:AddAmmoType("fas2_ammo_556x45", "5.56x45MM Ammo", "models/Items/combine_rifle_cartridge01.mdl", 160, 60)
+GAMEMODE:AddAmmoType("5.45x39MM", "5.45x39MM Ammo", "models/Items/combine_rifle_cartridge01.mdl", 160, 60)
+GAMEMODE:AddAmmoType("7.62x39MM", "7.62x39MM Ammo", "models/Items/combine_rifle_cartridge01.mdl", 160, 60)
+GAMEMODE:AddAmmoType("5.56x45MM", "5.56x45MM Ammo", "models/Items/combine_rifle_cartridge01.mdl", 160, 60)
 
 -- Shotgun Ammo Types
-AddAmmoType("fas2_ammo_12gauge", "12 Guage Ammo", "models/Items/BoxBuckshot.mdl", 180, 32)
+AddAmmoType("12 Gauge", "12 Gauge Ammo", "models/Items/BoxBuckshot.mdl", 180, 32)
 
 -- Shotgun With Slug Ammo Type
-GAMEMODE:AddAmmoType("fas2_ammo_23x75", "23x75 Ammo", "models/Items/BoxBuckshot.mdl", 180, 20)
+GAMEMODE:AddAmmoType("23x75MMR", "23x75 Ammo", "models/Items/BoxBuckshot.mdl", 180, 20)
 
 -- Sniper Rifle Ammo Types
-GAMEMODE:AddAmmoType("fas2_ammo_762x51", "7.62x51MM Ammo", "models/Items/BoxMRounds.mdl", 200, 40)
-GAMEMODE:AddAmmoType("fas2_ammo_50bmg", ".50 BMG Ammo", "models/Items/BoxMRounds.mdl", 200, 40)
-
--- Explosive Ammo Types
-GAMEMODE:AddAmmoType("fas2_ammo_m67", ".50 BMG Ammo", "models/Items/BoxMRounds.mdl", 240, 12)
+GAMEMODE:AddAmmoType("7.62x51MM", "7.62x51MM Ammo", "models/Items/BoxMRounds.mdl", 200, 40)
+GAMEMODE:AddAmmoType(".50 BMG", ".50 BMG Ammo", "models/Items/BoxMRounds.mdl", 200, 40)
