0697cdf3d2b70fb6ae9280e81f34ca5f2a6ec24f
IcyEngine <einar_d@msn.com>
2014-12-10 14:15:36 +0100

    Update skills.lua

--- ZombieRP v2/skills.lua
+++ ZombieRP v2/skills.lua
@@ -12,6 +12,28 @@ MaxRank = 1,
 x = 320,
 y = 40,
 func = function(ply) if ply:Health() == 75 then ply:SetHealth(100) end end
+
+}
+
+SKILLTREE.Config.Skills[2] = {
+Name = "Hard Skin",
+Type = "spawn",
+Desc = "You have learned to take more hits (Start with 10Armor)",
+MaxRank = 1,
+x = 320,
+y = 80,
+func = function(ply) if ply:Armor() == 0 then ply:SetArmor(10) end end
+}
+
+SKILLTREE.Config.Skills[3] = {
+Name = "New Shoes",
+Type = "spawn",
+Desc = "You have bought new choose to handle the wildness terraine +2 walkspeed +3 runspeed",
+MaxRank = 1,
+x = 320,
+y = 120,
+func = function(ply) ply:SetWalkSpeed(122) end
+func = function(ply) ply:SetRunSpeed(223) end
 }
 
 print("SkillTree Config Loaded")
