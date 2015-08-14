8b3f87b7dfa00370c6969f147565cb11b5b011cf
Devocity <chajecraft@hotmail.com>
2015-08-14 17:55:07 +0200

    Update vehicles.lua

--- ZombieRP-v3/customthings/vehicles.lua
+++ ZombieRP-v3/customthings/vehicles.lua
@@ -11,14 +11,14 @@ http://wiki.darkrp.com/index.php/DarkRP:Vehicles
 
 Add vehicles under the following line:
 ---------------------------------------------------------------------------]]
-DarkRP.createVehicle({
-    name = "Crown Vic",
-    model = "models/jessev92/blackmesa/veh_crownvic_chassis.mdl",
-    price = 3500,
-	allowed = {TEAM_ENGINEER}
-})
+
+// #NoSimplerr#
+
+MemberRanks = {"Member", "Trusted", "donator", "Owner", "superadmin", "admin"}
+timer.Simple(1, function() 
 DarkRP.createVehicle({
     name = "Pickup",
+	label = "Pickup truck",
     model = "models/jessev92/blackmesa/veh_pickup_chassis.mdl",
     price = 4000,
 	allowed = {TEAM_ENGINEER}
@@ -27,22 +27,25 @@ DarkRP.createVehicle({
     name = "Jeep",
     model = "models/jessev92/blackmesa/veh_jeep_chassis.mdl",
     price = 4000,
-	allowed = {TEAM_POLICE}
+	allowed = {TEAM_POLICE, TEAM_MILITARY, TEAM_CHIEF}
 })
 DarkRP.createVehicle({
     name = "Junker1",
+	label = "Scrap Car (#1)",
     model = "models/props_vehicles/car002b.mdl",
     price = 3000,
 	allowed = {TEAM_ENGINEER}
 })
 DarkRP.createVehicle({
     name = "Junker2",
+	label = "Scrap Car (#2)",
     model = "models/props_vehicles/car003b.mdl",
     price = 3000,
 	allowed = {TEAM_ENGINEER}
 })
 DarkRP.createVehicle({
     name = "Junker3",
+	label = "Scrap Car (#3)",
     model = "models/props_vehicles/van001a.mdl",
     price = 3000,
 	allowed = {TEAM_ENGINEER}
@@ -84,15 +87,16 @@ DarkRP.createVehicle({
 	allowed = {TEAM_ENGINEER}
 })
 DarkRP.createVehicle({
-    name = "PCJ-600",
-    model = "models/jessev92/blackmesa/veh_hmmwv_chassis.mdl",
-    price = 5000,
-	allowed = {TEAM_MILITARY}
-})
-DarkRP.createVehicle({
     name = "LAV-25",
     model = "models/jessev92/blackmesa/veh_lav_chassis.mdl",
 	customCheck = function(ply) return table.HasValue(MemberRanks, ply:GetUserGroup()) end,
     price = 15000,
 	allowed = {TEAM_MILITARY}
 })
+DarkRP.createVehicle({
+    name = "M1114",
+    model = "models/jessev92/blackmesa/veh_hmmwv_chassis.mdl",
+    price = 5000,
+	allowed = {TEAM_MILITARY}
+})
+end)
